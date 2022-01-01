c467fce4fab8a302bac9ca3d179050732c94c940
soc caroline <soc.caroline1991@gmail.com>
2022-01-01 17:53:00 +0100

    boucle carte produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,44 @@
 const sectionItems = document.getElementById("items");
-let donnees;
-let products = [];
-let imgProducts = document.createElement('img');
-let imgSrc;
-const imgHTML = document.querySelector('#items img');
+let productsData = [];
 
-// fonction : récupérer données sur l'API //
-function fetchProducts() {
-  fetch(`http://localhost:3000/api/products`)
+
+// fonction : récupérer données sur API
+async function fetchProducts() {
+  await fetch(`http://localhost:3000/api/products`)
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
     })
-
     .then((data) => {
-      console.log(data[0]);
-      //sectionItems.textContent = data[0];
+      productsData = data;
+      console.log(productsData);
     })
     .catch((erreur) => {
       console.log("Une erreur est survenue dans l'api");
     });
 }
 
-console.log();
-
-
-
-
-
-/*
-function displayProducts(){
-    sectionItems.innerHTML = products
-    .map (product) => {
-        let descrProduct = [];
-        
-        for (i=0; i<7; i++) {
-            let colors = product[`data${i}`]
-        }
-    }
+//fonction créer card pour chaque item
+ async function createCards() {
+  await fetchProducts()
+  for (i =0; i<productsData.length; i++ ){
+    sectionItems.innerHTML = productsData
+    .map(
+      (card) =>
+      `
+      <a href=${productsData[i].id}>
+        <article>
+          <img src=${productsData[i].imageUrl} alt=${productsData[i].altTxt}
+          />
+          <h3 class="productName">${productsData[i].name}</h3>
+          <p class="productDescription">${productsData[i].description}
+          </p>
+        </article>
+      </a>
+      `
+    )
+  }
 }
-*/
+createCards()
+
