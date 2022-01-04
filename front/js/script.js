2a532b97bd7187352f581f2ff81551f2931d67b3
soc caroline <soc.caroline1991@gmail.com>
2022-01-04 10:27:17 +0100

    factorisation page accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,46 +1,30 @@
 const sectionItems = document.getElementById("items");
-let productsData = [];
 
 // fonction : récupérer données sur API
-async function fetchProducts() {
-  await fetch(`http://localhost:3000/api/products`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      productsData = data;
-      console.log(productsData);
-    })
-    .catch((erreur) => {
-      console.log("Une erreur est survenue dans l'api");
-    });
-  }
+fetch("http://localhost:3000/api/products")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((productsData) => {
+    console.log(productsData);
+    for (data of productsData) {
+      sectionItems.innerHTML += `
+        <a href="./product.html?id=${data._id}">
+        <article>
+        <img src="${data.imageUrl}" alt="${data.altTxt}"
+        />
+        <h3 class="productName">${data.name}</h3>
+        <p class="productDescription">${data.description}
+        </p>
+        </article>
+        </a>
+        `;
+    }
+  })
+  .catch((erreur) => {
+    console.log("Une erreur est survenue dans l'api");
+  });
+
   
-
-  function formatCard(tableau, indice) {
-    return `
-    <a href="./product.html?id=${tableau[indice]._id}">
-    <article>
-    <img src="${tableau[indice].imageUrl}" alt="${tableau[indice].altTxt}"
-    />
-    <h3 class="productName">${tableau[indice].name}</h3>
-    <p class="productDescription">${tableau[indice].description}
-    </p>
-    </article>
-    </a>
-    `;
-  }
-
-  //fonction créer card pour chaque item
-async function createCards() {
-  await fetchProducts();
-  let display = "";
-  for (i =0; i < productsData.length; i++ ){
-    display += formatCard(productsData,i)
-  }
-  sectionItems.innerHTML = display
-}
-createCards();
-
