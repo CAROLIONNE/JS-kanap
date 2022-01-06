4fa8e29d746ced8829b6da78c708aedcbb255dbe
soc caroline <soc.caroline1991@gmail.com>
2022-01-06 15:32:48 +0100

    test couleur pour envoyer au panier

--- front/js/product.js
+++ front/js/product.js
@@ -5,8 +5,8 @@ const title = document.getElementById("title");
 const colorsElt = document.getElementById("colors");
 let productData = [];
 
+/////////----Recupérer ID dans URL----/////////
 
-//Recupérer ID dans URL
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 fetch(`http://localhost:3000/api/products/` + id)
@@ -25,18 +25,28 @@ fetch(`http://localhost:3000/api/products/` + id)
     console.log("Une erreur est survenue dans l'api");
   });
 
-//Affichage détails produit
+///////----Affichage détails produit----////////
 
 function detailProduct() {
-  let imgSrc = `<img src=${productData.imageUrl} alt=${productData.altTxt}/>`;
+  
+  /* test boucle couleur
   let displayColor = [];
   for (i = 0; i < productData.colors.length; i++) {
     displayColor += `<option value="${productData.colors[i]}">${productData.colors[i]}</option>`;
   }
-
   colorsElt.innerHTML = displayColor;
+  */
+  productData.colors.forEach((color) => {
+    let newOption = document.createElement("option");
+    newOption.innerHTML = `${color}`;
+    newOption.value = `${color}`;
+
+    let parentNode = document.querySelector("#colors");
+    parentNode.appendChild(newOption);
+  })
+
   title.innerHTML = productData.name;
-  img.innerHTML = imgSrc;
+  img.innerHTML = `<img src=${productData.imageUrl} alt=${productData.altTxt}/>`;
   price.innerHTML = productData.price;
   description.innerHTML = productData.description;
   document.title = productData.name;
@@ -47,27 +57,36 @@ function detailProduct() {
 // envoyer les données du panier
 document.getElementById("addToCart").addEventListener("click", (e) => {
   e.preventDefault();
-  //storage(productsCart);
+  //storage();
 });
 
+function colorValue (){
+  
+  let color= document.querySelector("option").parentNode.value;
+  let color2 = colorsElt.value;
+  let color3 = [];
+  for (i = 0; i < colorsElt.length; i++){
+    if (colorsElt[i].selected){
+      color3.push(colorsElt[i].value)
+    }
+  }
+  console.log(color);
+  console.log(color2);
+  console.log(color3);
+  
+}
 
+colorValue()
 
 let quantity = document.querySelector("input").value;
-let color=[] ;
-for (i = 0; i < colorsElt.length; i++){
-    if (colorsElt[i].selected){
-        color.push(colorsElt[i].value);
-    }
-}
-console.log(color);
+//let productsCart = [id, quantity,color];
 
-let productsCart = [id, quantity, color];
-
-console.log(productsCart);
+//console.log(productsCart);
 
 //localStorage.name = JSON.stringify(obj);//  chaines de cacratères pour local storage
 //JSON.parse(localStorage.name) // transformer en objet JS
 // parseInt string en Number
+
 /*
 function storage() {
   localStorage.cart = productsCart;
